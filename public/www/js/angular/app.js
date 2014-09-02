5fb801745bf130aa5722aab9b9488aa9fa523928
Carlos Castillo Sánchez <poleecastillo@gmail.com>
2014-09-02 16:44:06 -0500

    Ajustes menores, modulos para formularios

--- public/www/js/angular/app.js
+++ public/www/js/angular/app.js
@@ -13,18 +13,17 @@
             when('/search', {
                 templateUrl: 'www/search.html'
             }).
-            when('/sign_on', {
-                templateUrl: 'www/sign_on.html'
+            when('/sign_up', {
+                templateUrl: 'www/sign_up.html'
+            }).
+            when('/sign_in', {
+                templateUrl: 'www/sign_in.html'
             }).
             when('/doctor/:doc_name', {
                 templateUrl: 'www/doctor.html'
             }).
             /*when('/doctor/gregory_house#doc-book', {
                 templateUrl: 'www/doctor.html'
-            }).*/
-            /*when('/:PoleeCastillo/search_plan', {
-                templateUrl: 'www/plan_form.html',
-                controller: 'SliderController'
             }).*/
             otherwise({
                 redirectTo: '/404'
