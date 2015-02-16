da31a98430a313c725dc18e71c937db7efacaff4
Carlos Castillo Sánchez <poleecastillo@gmail.com>
2015-02-15 22:50:20 -0500

    Añadidos comentarios a módulos de creación de cuenta

--- public/www/js/angular/createDoctor.js
+++ public/www/js/angular/createDoctor.js
@@ -1,13 +1,16 @@
 var endpoint = "http://192.241.187.135:1414/api_1.0/";
 
+//Modulo y Controlador para crear cuenta de Doctor
 var createDoctor = angular.module('createDoctor', []);
 createDoctor.controller('DoctorSignUpController', ['$http', '$scope', 'PracticesService', '$location', '$anchorScroll', function($http, $scope, PracticesService, $location, $anchorScroll) {
+	//Cargar localidades de Bogotá
 	$scope.localidades = localidades;
 
 	this.practices = [];
 
 	var self = this;
 
+	//Cargar especialidades
 	var promiseGetAllPractices = PracticesService.getAll();
 	promiseGetAllPractices.then(function(response) {
 		console.log(response.data);
@@ -15,19 +18,20 @@ createDoctor.controller('DoctorSignUpController', ['$http', '$scope', 'Practices
 	});
 
 	var type = "Doctor";
+
+	//función para crear cuenta
 	this.signUp = function() {
-		//console.log('Entra a signUp');
 		var data1 = this.data;
+		//la contraseña y su confirmación se codifican en base 64
 		data1.password = btoa(data1.password);
 		data1.password_verify = btoa(data1.password_verify);
 		data1.practice_list = data1.practice_list.name;
 		data1.birthday = data1.birthday.getTime();
-		console.log(data1);
+
+		//Servicio POST para crear cuenta
 		$http.post(endpoint + type + '/Create', data1)
 			.success(function(data) {
 				if (!data.status) {
-					console.log("Paila, no se creó", data);
-					//console.log(JSON.stringify(data1));
 					swal({
 						title: "",
 						text: "Ha ocurrido un error, por favor inténtalo nuevamente.",
@@ -35,15 +39,12 @@ createDoctor.controller('DoctorSignUpController', ['$http', '$scope', 'Practices
 						confirmButtonText: "Aceptar",
 					});
 				} else {
-					// if successful, bind success message to message
-					console.log("Listo, creado" + data);
-					console.log(JSON.stringify(data1));
 					var doc = data.response;
 					var type = "doctor";
 					var email = btoa(doc.email);
+					//Redirección a formulario de confirmación de cuenta
 					window.location = "/#/account_confirmation/" + type + "/" + email;
 				}
 			});
-		//this.data = {};
 	};
 }]);
